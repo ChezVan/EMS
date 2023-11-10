2fcb9ce7f09afcaafc595c4ace00893232a95dee
ChezVan <chezney@thedigitalacademy.co.za>
2023-11-10 09:16:22 +0200

    modified client and admin

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './App.css'
+import Test from '../pages/test.jsx'
+import Client from '../../client/src/App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/test",
+    element: <Test/>,
+  },
+  {
+    path: "/client",
+    element: <Client/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
